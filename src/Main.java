f4cf3b90e2584d1e06aa22503ae0ee7da6667983
tunisiaa-guy <federico.toluzzo1@gmail.com>
2023-10-07 14:08:10 +0200

    Got the first method, reactionTime() to work

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 import java.awt.*;
-import java.awt.event.MouseEvent;
+import java.awt.event.InputEvent;
 
 public class Main {
     public static void main(String[] args){
@@ -9,12 +9,31 @@ public class Main {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+        reactionTime();
     }
 
+    public static void reactionTime(){
+        Robot robot = null;
+        try {
+            robot = new Robot();
+        } catch (AWTException e) {
+            throw new RuntimeException(e);
+        }
+        for(int i = 0; i < 5; i++){
+            robot.mouseMove(500, 500);
+            robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+            robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+            robot.delay(20);
+            Color red = robot.getPixelColor(500, 500);
+            while(true){
+                if(!robot.getPixelColor(500, 500).equals(red)){
+                    break;
+                }
+            }
+            robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+            robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+            robot.delay(20);
+        }
 
-    public void reactionTime(MouseEvent e) throws AWTException {
-        Robot robot = new Robot();
-        Color clickedColor = robot.getPixelColor(e.getX(),e.getY());
-        System.out.println(clickedColor.getRGB());
     }
 }
